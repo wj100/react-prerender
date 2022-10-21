9b236dde67897c33cf7260c48c71741e6a9fe63a
wangjun <wb.wangjun21@mesg.corp.netease.com>
2022-10-21 10:58:43 +0800

    redux Toolkit

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { ageIncremented } from '../store/slices/userSlice';
 function About() {
+  const counter = useSelector(state => state.counter.value);
+  const age = useSelector(state => state.user.age);
+  const dispatch = useDispatch();
+
+  const userAgeIncremented = () => {
+    dispatch(ageIncremented(5));
+  };
   return (
-    <p className='meng'>
-      about
+    <p className="meng">
+      {age}-
+      <span onClick={userAgeIncremented}>btn</span>
       <Outlet />
     </p>
   );
